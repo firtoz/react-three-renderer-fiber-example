3aa9d7ab20c576c3de54a201a99dc06f5fb69a38
Firtina Ozbalikci <firtina14@gmail.com>
2018-10-14 16:25:53 +0100

    Initial commit of Simple example

---
+++ index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Simple from "./src/Simple";
+
+ReactDOM.render(<Simple
+  width={500}
+  height={600}
+/>, document.getElementById("container"));
